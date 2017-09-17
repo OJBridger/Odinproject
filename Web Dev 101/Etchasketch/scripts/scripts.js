dcbb987766eebd5df5114367e8bb749ee68dbd08
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2017-09-17 12:47:54 +0100

    Etchasketch

--- Web Dev 101/Etchasketch/scripts/scripts.js
+++ Web Dev 101/Etchasketch/scripts/scripts.js
@@ -2,7 +2,16 @@ $(document).ready(function() {
 	for(i = 1; i <= 256; i++) {
 		$('.container').append('<div></div>');
 		console.log('Div ' + i + ' Appended');
-		$('.container').find('div').addClass('myclass');
+		$('.container').find('div').addClass('default');
 	}
 	console.log('All Divs allocated Class');
+
+
+	$('.container').find('div').on('mouseenter', function() {
+		$(this).addClass('highlighted');
+	});
+	$('.container').find('div').on('mouseleave', function() {
+		$(this).removeClass('highlighted');
+		$(this).addClass('trail');
+	});
 });
