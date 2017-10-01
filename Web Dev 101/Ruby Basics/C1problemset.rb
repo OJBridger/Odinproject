117bac23a1253d84a94adc8a272ffce90e5eadbd
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2017-10-01 11:23:45 +0100

    Ruby basics

---
+++ Web Dev 101/Ruby Basics/C1problemset.rb
@@ -0,0 +1,47 @@
+puts "Hours in a year"
+
+hours_in_day = 24
+days_in_year = 365
+hours_in_year = hours_in_day * days_in_year
+
+puts hours_in_year
+
+
+puts "Minutes in Decade"
+
+minutes_in_year = hours_in_year * 60
+minutes_in_decade = minutes_in_year * 10
+
+puts minutes_in_decade
+
+
+puts "How many Seconds old are you"
+
+age = 23
+seconds_in_year = minutes_in_year * 60
+seconds_old = minutes_in_year * age
+
+puts seconds_old
+
+
+puts "How many chocolates do you hope to eat in your life"
+
+choc_per_day = 1
+life_expectancy = 80
+days_alive = life_expectancy * days_in_year
+choc_in_life = choc_per_day * days_alive
+
+puts choc_in_life
+
+puts "How old is 1298000000 seconds"
+
+age_in_seconds = 1298000000
+age_in_minutes = age_in_seconds / 60
+age_in_hours = age_in_minutes / 60
+age_in_days = age_in_hours / 24
+age_in_years = age_in_days / 365
+
+puts age_in_years
+
+
+
