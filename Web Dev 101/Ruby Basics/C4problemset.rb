85a31fb4bf2a6c407ec97c2936450efdbbedacb7
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2017-10-01 11:42:24 +0100

    Chapter 4 Problem Set

---
+++ Web Dev 101/Ruby Basics/C4problemset.rb
@@ -0,0 +1,13 @@
+puts 'Hello what is your first name?'
+first_name = gets.chomp
+puts 'Do you have a middle name?'
+middle_name = gets.chomp
+puts 'What is your surname?'
+last_name = gets.chomp
+
+puts 'Thanks for that ' + first_name + '. We can see your full name is ' + first_name + ' ' + middle_name + ' ' + last_name
+
+puts 'What is your favourite number?'
+fav_num = gets.chomp
+new_fav_num = fav_num.to_i + 1
+puts 'Not bad... but how about ' + new_fav_num.to_s + '?'
