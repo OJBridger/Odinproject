11eb1c1414c4ee77d685b66866cf2e76cdc52d1f
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2017-10-13 19:40:28 +0100

    ruby work

--- Web Dev 101/Ruby Basics/C5problemset.rb
+++ Web Dev 101/Ruby Basics/C5problemset.rb
@@ -25,7 +25,7 @@ puts 'WHAT? ' + answer + ' HAHAHAHHA'
 
 lineWidth = 20
 puts 'Table of Contents'.center(lineWidth)
-
+puts ''
 puts 'Chapter 1:  Numbers'.ljust(lineWidth) +     'page 1'.rjust(lineWidth)
 puts 'Chapter 2:  Letters'.ljust(lineWidth) +    'page 72'.rjust(lineWidth)
 puts 'Chapter 3:  Variables'.ljust(lineWidth) + 'page 118'.rjust(lineWidth)
