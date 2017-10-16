3222b8a21ff0ed279afe4eeca91082273bb43c43
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2017-10-16 11:17:55 +0100

    ruby C7

---
+++ Web Dev 101/Ruby Basics/C7problemset.rb
@@ -0,0 +1,27 @@
+#Sorting words
+
+done = ''
+words = []
+while done != 'finished'
+	puts 'Input your words to be sorted. Type "finished" to stop'
+	word_input = gets.chomp
+	words.push word_input
+	if word_input == 'finished'
+		done = 'finished'
+	end
+end
+
+puts ''
+
+puts 'Your Words now in alphabetical order are:'
+puts words.sort
+
+# Table of Contents
+toc = ['Table of Contents', 'Chapter 1: Getting Started', 'page 1','Chapter 2: Numbers','page 9',
+'Chapter 3: Letters','page 13']
+page_width = 60
+puts (toc[0].center(page_width))
+puts ''
+puts (toc[1].ljust(page_width/2) + toc[2].rjust(page_width/2))
+puts (toc[3].ljust(page_width/2) + toc[4].rjust(page_width/2))
+puts (toc[5].ljust(page_width/2) + toc[6].rjust(page_width/2))
