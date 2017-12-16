597f39d303a0f926ac2eabef06eaf47465e7d8eb
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2017-12-16 20:41:10 +0000

    Work on new JS curriculum

---
+++ Web Dev 101/Javascript Fundamentsals/JSFundamentals3.js
@@ -0,0 +1,17 @@
+var add7 = function(num) {
+	console.log(num + 7)
+}
+
+var multiply = function(num1, num2) {
+	console.log(num1 * num2)
+}
+
+var capatalize = function(str) {
+	console.log(str.charAt(0).toUpperCase() + str.slice(1))
+}
+
+var lastLetter = function(str) {
+	console.log(str.charAt(str.length -1))
+}
+
+lastLetter("abcd")
