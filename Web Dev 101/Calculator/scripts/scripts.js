52c3c1f5b54771ca8f0c144d1caba87209f78220
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2018-04-03 14:45:12 +0100

    Calcualtor script, style and layout

---
+++ Web Dev 101/Calculator/scripts/scripts.js
@@ -0,0 +1,20 @@
+//Add
+function sumAdd(a, b) {
+	return a + b
+};
+//Subtract
+function sumSubtract(a, b) {
+	return a - b
+};
+//Multiply
+function sumMultiply(a, b) {
+	return a * b
+};
+//Divide
+function sumDivide(a, b) {
+	return a / b
+};
+//Operator
+function operate(func, num1, num2) {
+	return func(num1, num2)
+}
