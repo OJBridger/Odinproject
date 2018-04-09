8a478e08a86938d81618212326a4be54bb50c480
Oliver Suchanek <oliversuchanek@hotmail.co.uk>
2018-04-09 08:34:59 +0100

    Logic for buttons - Calculator App

--- Web Dev 101/Calculator/scripts/scripts.js
+++ Web Dev 101/Calculator/scripts/scripts.js
@@ -18,7 +18,81 @@ function sumDivide(a, b) {
 function operate(func, num1, num2) {
 	return func(num1, num2)
 }
+//Variable
+let displayVariable = 0;
+//Logic for populating the display box
+function showResult() {
 
-document.getElementById().onclick = function() {
+	const displayBox = document.querySelector("#displayVarible");
+	displayBox.textContent = displayVariable
 
 };
+
+
+//Button Logic
+document.getElementById('numBut1').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut2').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut3').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut4').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut5').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut6').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut7').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut8').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut9').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numBut0').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numButPlus').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numButMinus').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numButDivide').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numButTimes').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numButClear').onclick = function() {
+	alert('Test');
+};
+
+document.getElementById('numButEquals').onclick = function() {
+	alert('Test');
+};
+
+
+
